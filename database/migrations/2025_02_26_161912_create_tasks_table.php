59d240da4dc2f63f5c20bd66435b48f76c6c6d08
djamioufadebi <djamioufadebi1@gmail.com>
2025-04-18 15:59:48 +0100

    mise à jour

--- database/migrations/2025_02_26_161912_create_tasks_table.php
+++ database/migrations/2025_02_26_161912_create_tasks_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description')->nullable();
             $table->date('due_date')->nullable();
-            $table->enum('status', ['en_attente', 'en_cours', 'terminé'])->default('en_attente');
+            $table->string('status')->default('en_attente');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
